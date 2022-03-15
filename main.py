339cff93301c727e0c2fc019aafb77c868864cf9
Nerpheus <goetze.nico@hotmail.de>
2022-03-15 17:27:39 +0100

    countries und teams wieder aktiviert. Fehler in Datenbank gefixt.

--- main.py
+++ main.py
@@ -302,9 +302,9 @@ def test():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # countries()
+    countries()
     leagues()
-    # teams()
+    teams()
     # fixtures()
     # events()
     # odds()
