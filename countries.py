2c08c1c268f47e613662b4608ddc84e1d0705c33
Nerpheus <goetze.nico@hotmail.de>
2022-03-21 13:13:40 +0100

    fixes

--- countries.py
+++ countries.py
@@ -32,38 +32,39 @@ def countries():
             'x-rapidapi-host': 'v3.football.api-sports.io'
         }
 
-        retries = 0
+        retries = 1
         success = False
 
         while not success and retries <= 5:
             try:
                 response = requests.get(url=url, headers=headers, timeout=60)
                 success = response.ok
-                if success and retries > 0:
+                if success and retries > 1:
                     logging.info("solved!")
-            except requests.exceptions.Timeout as timeout:
+            except requests.exceptions.Timeout:
                 wait = retries * 30
                 logging.info("Timeout Error! Try again in {} seconds.".format(wait))
-                # logging.info(timeout)
-                logging.info(response.status_code)
-                logging.info(response.json())
                 time.sleep(wait)
                 retries += 1
+            else:
+                errors = response.json()['errors']
+                if not errors:
+                    data = response.json()['response']
 
-        data = response.json()['response']
+                    for d in data:
+                        country = {'name': d['name'].replace('-', ' '), 'slug': d['name'].lower()}
+                        if d['code'] is not None:
+                            country['code2'] = d['code']
 
-        for d in data:
-            country = {'name': d['name'].replace('-', ' '), 'slug': d['name'].lower()}
-            if d['code'] is not None:
-                country['code2'] = d['code']
+                        if d['flag'] is not None:
+                            country['flag'] = 'country-flags/{}'.format(d['flag'].split('/')[-1])
+                            r = requests.get(d['flag'], allow_redirects=True)
+                            open(country['flag'], 'wb').write(r.content)
 
-            if d['flag'] is not None:
-                country['flag'] = '/home/nico/api-football/country-flags/{}'.format(d['flag'].split('/')[-1])
-                r = requests.get(d['flag'], allow_redirects=True)
-                open(country['flag'], 'wb').write(r.content)
-
-            print(country)
-            mydb.updateCountry(country)
+                        print(country)
+                        mydb.updateCountry(country)
+    else:
+        logging.info("Requests für heute aufgebraucht.")
 
 
 # Press the green button in the gutter to run the script.
